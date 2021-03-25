1597e0c2a1e6bf39590080ad5718aab2d0906a92
Kristen <43305741+KISMET-INC@users.noreply.github.com>
2021-03-25 13:45:55 -0700

    honorable completed

--- client/src/components/Character_Card.js
+++ client/src/components/Character_Card.js
@@ -10,10 +10,10 @@ const Character_Card = props => {
 
     return( 
         <>
-        <div id = "character_wrapper">
+        <div id = "character_wrapper" class = 'flex'>
             <img src = "https://i.pinimg.com/originals/8e/4a/a4/8e4aa4690779c21a6b9c0927f63b8d4b.jpg" />
             <button>Vote</button>
-            <h4> Tyler Durden</h4>
+            <h4>TYLER DURDEN</h4>
             <p>Rank 5 | Votes: 12 </p>
             <h5>Fight Club</h5>
         </div>
--- client/src/components/Honorable.js
+++ client/src/components/Honorable.js
@@ -1,18 +1,37 @@
 import { Link } from '@reach/router';
 import React from 'react';
 import '../App.css';
+import '../static/css/Honorable.css'
 import Character_Card from './Character_Card';
+import Slider from 'infinite-react-carousel';
 
 
 
 const Honorable = props => {
-
+    const settings = {
+        dots: true,
+        slidesToShow: 4,
+        arrows:false,
+    };
 
     return(
         <div id = "honorable_wrapper">
-            <h3>Honorable Mention <Link to= "/search">Click to see all</Link></h3>
-            <Character_Card />
-        </div>
+            <h3> ★ Honorable Mentions ★ <Link to= "/search"><span class= 'link'>Click to see all</span></Link></h3>
+    
+
+                <Slider {...settings}>
+                    <Character_Card />
+                    <Character_Card />
+                    <Character_Card />
+                    <Character_Card />
+                    <Character_Card />
+                    <Character_Card />
+                    <Character_Card />
+                    <Character_Card />
+                    <Character_Card />
+                </Slider>
+                </div>
+
 
     )
 }

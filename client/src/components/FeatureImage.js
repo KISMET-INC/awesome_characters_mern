0193eff59683f398883b4a5c786c9444489442c7
Kristen <43305741+KISMET-INC@users.noreply.github.com>
2021-04-02 16:18:58 -0700

    Main Feature mostly responsive

--- client/src/components/FeatureImage.js
+++ client/src/components/FeatureImage.js
@@ -10,10 +10,19 @@ const FeatureImage = ({url, alt, goto_view}) => {
 
     return (
 
-        <div id= 'Feature_Image'>
+        <section id= 'Feature_Image'>
             <img onClick = {goto_view} src = {url}  alt = {alt} />
+            <div className = 'image_info'>
+                <div className = 'background'>
+                    <h3>Rank  1</h3>
+                    <h3>Tyler Duren</h3>
+                    <h4>Votes: 5 | VOTE!</h4>
+                    <h5 onClick = {goto_view}> Click to View  |  Click to Edit </h5>
+                </div>
+            </div>
+        </section>
 
-        </div>
+        
     )
 }
 
